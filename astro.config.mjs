c604a30f292cee7fb0377e2472de1a5f32e83a2b
dhimankeshav201 <dhimankeshav201@gmail.com>
2025-01-12 16:52:36 +0000

    feat: add gh pages integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,4 +8,6 @@ import solidJs from "@astrojs/solid-js"
 export default defineConfig({
   site: "https://astro-sphere-demo.vercel.app",
   integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
+  site: "https://Keshav-writes-code.github.io",
+  base: "Portfolio",
 })
