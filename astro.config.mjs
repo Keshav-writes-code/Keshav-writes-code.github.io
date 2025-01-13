dd55c756abe68cc73f3930c559c4c27ed9dabe9a
dhimankeshav201 <dhimankeshav201@gmail.com>
2025-01-13 03:41:29 +0000

    fix: make base the root githuburl

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,7 @@ import solidJs from "@astrojs/solid-js"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-sphere-demo.vercel.app",
   integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
   site: "https://Keshav-writes-code.github.io",
-  base: "portfolio-temp",
+  base: "/",
 })
